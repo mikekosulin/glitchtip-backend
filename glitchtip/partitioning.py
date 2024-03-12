9e301138cfe020337ee67d3d1036b680f7e05815
David Burke <david@burkesoftware.com>
2024-03-12 09:09:59 -0400

    Disable not yet used partitioning

--- glitchtip/partitioning.py
+++ glitchtip/partitioning.py
@@ -2,7 +2,8 @@ from dateutil.relativedelta import relativedelta
 from django.conf import settings
 
 from apps.issue_events.models import IssueEvent, IssueTag
-from apps.performance_events import TransactionGroup
+
+# from apps.performance_events import TransactionGroup
 from psqlextra.partitioning import (
     PostgresCurrentTimePartitioningStrategy,
     PostgresPartitioningManager,
@@ -15,18 +16,18 @@ issue_strategy = PostgresCurrentTimePartitioningStrategy(
     count=7,
     max_age=relativedelta(days=settings.GLITCHTIP_MAX_EVENT_LIFE_DAYS),
 )
-transaction_strategy = PostgresCurrentTimePartitioningStrategy(
-    size=PostgresTimePartitionSize(days=1),
-    count=7,
-    max_age=relativedelta(days=settings.GLITCHTIP_MAX_TRANSACTION_EVENT_LIFE_DAYS),
-)
+# transaction_strategy = PostgresCurrentTimePartitioningStrategy(
+#     size=PostgresTimePartitionSize(days=1),
+#     count=7,
+#     max_age=relativedelta(days=settings.GLITCHTIP_MAX_TRANSACTION_EVENT_LIFE_DAYS),
+# )
 
 manager = PostgresPartitioningManager(
     [
         PostgresPartitioningConfig(model=IssueEvent, strategy=issue_strategy),
         PostgresPartitioningConfig(model=IssueTag, strategy=issue_strategy),
-        PostgresPartitioningConfig(
-            model=TransactionGroup, strategy=transaction_strategy
-        ),
+        # PostgresPartitioningConfig(
+        #     model=TransactionGroup, strategy=transaction_strategy
+        # ),
     ]
 )
